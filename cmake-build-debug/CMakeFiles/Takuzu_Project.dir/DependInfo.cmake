010185724955fec21e1bc65d54b7b943e7b01347
7ntys <61086353+7ntys@users.noreply.github.com>
2022-05-12 16:35:25 +0200

    return the grid

---
+++ cmake-build-debug/CMakeFiles/Takuzu_Project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/julienleber/CLION C project/Takuzu-Project1/function.c" "CMakeFiles/Takuzu_Project.dir/function.c.o" "gcc" "CMakeFiles/Takuzu_Project.dir/function.c.o.d"
+  "/Users/julienleber/CLION C project/Takuzu-Project1/main.c" "CMakeFiles/Takuzu_Project.dir/main.c.o" "gcc" "CMakeFiles/Takuzu_Project.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Takuzu_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Takuzu_Project.
+# This may be replaced when dependencies are built.
